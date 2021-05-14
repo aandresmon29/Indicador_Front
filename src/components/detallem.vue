14415033f0de2d5251c4a3fbb00ef4714788ae86
aandresmon29 <71345919+aandresmon29@users.noreply.github.com>
2021-05-14 11:23:42 -0500

    Update with nuw settings for detail

--- src/components/detallem.vue
+++ src/components/detallem.vue
@@ -12,7 +12,7 @@
   <nav3>
     <h2>Datos</h2>
     <label1 for="fgerencia"> {{calculo}} </label1>
-    <br><br>
+    <br>
     <h2>Observación</h2>
     <label1 for="fgerencia"> {{comentario}} </label1>
     <br>
@@ -72,3 +72,92 @@ export default {
 </script>
 
 <style>
+#maindet h3{
+  display:inline-flex;
+  justify-content:center;
+  align-items:center;
+  height: 10px;
+  width: 100%;
+}
+#maindet h7{
+  display:inline-flex;
+  justify-content:center;
+  align-items:center;
+  text-align: center;
+  width: 100%;
+  height: 15px;
+}
+#maindet nav2{
+  margin: 0px;
+  width: 100%;
+  height: 20vh;
+  text-align: center;
+  color: black;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  float: left;
+}
+#maindet nav2 label1{
+  background: white;
+  width:10px;
+  height:50px;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  text-align: left;
+}
+#maindet nav2 h4 {
+  -moz-border-radius: 50%;
+  -webkit-border-radius: 50%;
+  border-radius: 50%;
+  background:rgb(60, 255, 0);
+  width:50px;
+  height:50px;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+}
+#maindet nav2 h5 {
+  -moz-border-radius: 50%;
+  -webkit-border-radius: 50%;
+  border-radius: 50%;
+  background: yellow;
+  width:50px;
+  height:50px;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+}
+#maindet nav2 h6 {
+  -moz-border-radius: 50%;
+  -webkit-border-radius: 50%;
+  border-radius: 50%;
+  background: red;
+  width:50px;
+  height:50px;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+}
+#maindet nav3{
+  margin: 0px;
+  width: 100%;
+  height: 20px;
+  min-height: 20px; 
+  text-align: center;
+  color: black;
+  display: inline-block;
+  align-items: center;
+  justify-content: center;
+}
+#maindet nav3 h2{
+  height: 20px;
+}
+#maindet nav3 h2{
+  height: 20px;
+}
+#maindet nav3 form{
+  margin: 20px;
+}
+</style>
